c6608a16d6257ec45cdc70e407c17888cb2a14da
João Volpato <volpatocode@gmail.com>
2022-11-08 21:54:49 -0300

    feat: logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function index() {
+  return (
+    <Link className="text-3xl text-slate-100 font-bold tracking-wide cursor-pointer" href="/">
+      NFThink
+    </Link>
+  );
+}
