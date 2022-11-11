2d605e86c8d8e16f84c2deebbe77711079a148e2
João Volpato <volpatocode@gmail.com>
2022-11-10 22:16:06 -0300

    feat: home page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,44 @@ export default function Home() {
       </Head>
 
       <Navbar />
+      <div className="container mx-auto w-full min-h-screen flex flex-row justify-between">
+        <div className=" w-1/3 flex flex-col gap-3">
+          <h1 className="font-extrabold text-6xl">
+            Collect and Sell your <span className="text-pink-700">awesome</span>{" "}
+            NFTs
+          </h1>
+          <p className="text-md text-white-600">
+            Welcome to the future, you can buy and sell awesome artwork to
+            enjoy.
+          </p>
+          <div className="flex flex-row gap-5">
+            <button className="w-1/2 px-4 py-2 bg-purple-600">
+              Explore Now
+            </button>
+            <button
+              className="w-1/2 px-4 py-2 bg-transparent border-white-800"
+              style={{ border: "1px solid" }}
+            >
+              Create
+            </button>
+          </div>
+          <div className="flex flex-row gap-2 justify-between items-center">
+            <div>
+              <h4 className="text-4xl font-semibold">50K+</h4>
+              <p className="text-lg text-white-700">Artwork</p>
+            </div>
+            <div>
+              <h4 className="text-4xl font-semibold">100K+</h4>
+              <p className="text-lg text-white-700">Auction</p>
+            </div>
+            <div>
+              <h4 className="text-4xl font-semibold">10K+</h4>
+              <p className="text-lg text-white-700">Creator</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2">carrousel</div>
+      </div>
       <Footer />
     </>
   );
