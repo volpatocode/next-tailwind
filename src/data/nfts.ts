d45ddad64e8b93c6ccd8f73cb83d11d343eede11
João Volpato <volpatocode@gmail.com>
2022-11-19 17:25:39 -0300

    feat: nft fake data

---
+++ src/data/nfts.ts
@@ -0,0 +1,42 @@
+export const nfts = [
+  {
+    id: "1",
+    name: "Punker",
+    type: "Artwork",
+    author: "Vanessa Almond",
+    users: "122",
+    img: "nft1.png",
+  },
+  {
+    id: "2",
+    name: "Spacesoldier",
+    type: "Gamified",
+    author: "Adam Heath",
+    users: "77",
+    img: "nft2.png",
+  },
+  {
+    id: "3",
+    name: "Crazy Bear",
+    type: "Artwork",
+    author: "Gregory Kibbe",
+    users: "99",
+    img: "nft3.png",
+  },
+  {
+    id: "4",
+    name: "Pedro",
+    type: "Lorem",
+    author: "Louis Pitto",
+    users: "44",
+    img: "nft4.png",
+  },
+  {
+    id: "5",
+    name: "Moonkey",
+    type: "Lorem",
+    author: "Mary Weiss",
+    users: "35",
+    img: "nft5.png",
+  },
+];
